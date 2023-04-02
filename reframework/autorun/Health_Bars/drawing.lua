f851790d8940eee803aef12cf39fcf773bc7ef78
GreenComfyTea <klunics97@gmail.com>
2023-04-02 15:31:21 +0300

    Implement label drawing and add bar fill direction

--- reframework/autorun/Health_Bars/drawing.lua
+++ reframework/autorun/Health_Bars/drawing.lua
@@ -75,6 +75,15 @@ function this.scale_color_opacity(color, scale)
 	return this.argb_to_color(new_alpha, red, green, blue);
 end
 
+function this.scale_label_opacity(label, scale)
+	if label == nil or scale == nil or not label.visibility then
+		return;
+	end
+
+	label.color = this.scale_color_opacity(label.color, scale);
+	label.shadow.color = this.scale_color_opacity(label.shadow.color, scale);
+end
+
 function this.scale_bar_opacity(bar, scale)
 	if bar == nil or scale == nil or not bar.visibility then
 		return;
@@ -84,14 +93,50 @@ function this.scale_bar_opacity(bar, scale)
 	bar.colors.background = this.scale_color_opacity(bar.colors.background, scale);
 end
 
-function this.scale_label_opacity(label, scale)
-	if label == nil or scale == nil or not label.visibility then
+function this.draw_label(label, position, opacity_scale, ...)
+	if label == nil or not label.visibility then
 		return;
 	end
 
-	label.color = this.scale_color_opacity(label.color, scale);
-	label.shadow.color = this.scale_color_opacity(label.shadow.color, scale);
+	local text = string.format(label.text_format, table.unpack({...}));
+
+	if text == "" then
+		return;
+	end
+
+	local position_x = position.x + label.offset.x;
+	local position_y = position.y + label.offset.y;
+
+	local use_d2d = d2d ~= nil and config.current_config.settings.use_d2d_if_available;
+
+	if label.shadow.visibility then
+		local new_shadow_color = label.shadow.color;
+
+		if opacity_scale < 1 then
+			new_shadow_color = this.scale_color_opacity(new_shadow_color, opacity_scale);
+		end
+
+		if use_d2d then
+			d2d.text(this.font, text, position_x + label.shadow.offset.x, position_y + label.shadow.offset.y, new_shadow_color);
+		else
+			new_shadow_color = this.argb_color_to_abgr_color(new_shadow_color);
+			draw.text(text, position_x + label.shadow.offset.x, position_y + label.shadow.offset.y, new_shadow_color);
+		end
+	end
+
+	local new_color = label.color;
+	if opacity_scale < 1 then
+		new_color = this.scale_color_opacity(new_color, opacity_scale);
+	end
+
+	if use_d2d then
+		d2d.text(this.font, text, position_x, position_y, new_color);
+	else
+		new_color = this.argb_color_to_abgr_color(new_color);
+		draw.text(text, position_x, position_y, new_color);
+	end
 end
+
 
 function this.draw_bar(bar, position, opacity_scale, percentage)
 	if bar == nil or not bar.visibility then
@@ -108,6 +153,7 @@ function this.draw_bar(bar, position, opacity_scale, percentage)
 
 	local outline_visibility = bar.outline.visibility;
 	local style = bar.outline.style; -- Inside/Center/Outside
+	local fill_direction = bar.settings.fill_direction; -- Left to Right/Right to Left/Top to Bottom/Bottom to Top
 
 	local outline_thickness = bar.outline.thickness;
 	if not outline_visibility then
@@ -132,9 +178,20 @@ function this.draw_bar(bar, position, opacity_scale, percentage)
 	local position_x = 0;
 	local position_y = 0;
 
-	local foreground_width = 0;
-	local background_width = 0;
+	local width = 0;
 	local height = 0;
+
+	local foreground_width = 0;
+	local foreground_height = 0;
+
+	local background_width = 0;
+	local background_height = 0;
+
+	local foreground_shift_x = 0;
+	local foreground_shift_y = 0;
+
+	local background_shift_x = 0;
+	local background_shift_y = 0;
 
 	if style == "Inside" then
 		outline_position_x = position.x + bar.offset.x + half_outline_thickness;
@@ -146,10 +203,7 @@ function this.draw_bar(bar, position, opacity_scale, percentage)
 		position_x = outline_position_x + half_outline_thickness + outline_offset;
 		position_y = outline_position_y + half_outline_thickness + outline_offset;
 
-		local width = outline_width - outline_thickness - outline_offset - outline_offset;
-		foreground_width = width * percentage;
-		background_width = width - foreground_width;
-
+		width = outline_width - outline_thickness - outline_offset - outline_offset;
 		height = outline_height - outline_thickness - outline_offset - outline_offset;
 
 	elseif style == "Center" then
@@ -162,27 +216,58 @@ function this.draw_bar(bar, position, opacity_scale, percentage)
 		position_x = outline_position_x + half_outline_thickness + outline_offset;
 		position_y = outline_position_y + half_outline_thickness + outline_offset;
 
-		local width = outline_width - outline_thickness - outline_offset - outline_offset;
-		foreground_width = width * percentage;
-		background_width = width - foreground_width;
-
+		width = outline_width - outline_thickness - outline_offset - outline_offset;
 		height = outline_height - outline_thickness - outline_offset - outline_offset;
 
-	else
+	else -- Outside
 		position_x = position.x + bar.offset.x;
 		position_y = position.y + bar.offset.y;
 
-		local width = bar.size.width;
+		width = bar.size.width;
 		height = bar.size.height;
-
-		foreground_width = width * percentage;
-		background_width = width - foreground_width;
 
 		outline_position_x = position_x - half_outline_thickness - outline_offset;
 		outline_position_y = position_y - half_outline_thickness - outline_offset;
 
 		outline_width = width + outline_thickness + outline_offset + outline_offset;
 		outline_height = height + outline_thickness + outline_offset + outline_offset;
+	end
+
+	if fill_direction == "Right to Left" then
+		foreground_width = width * percentage;
+		foreground_height = height;
+
+		background_width = width - foreground_width;
+		background_height = height;
+
+		foreground_shift_x = background_width;
+
+	elseif fill_direction == "Top to Bottom" then
+		foreground_width = width;
+		foreground_height = height * percentage;
+
+		background_width = width;
+		background_height = height - foreground_height;
+
+		background_shift_y = foreground_height;
+
+	elseif fill_direction == "Bottom to Top" then
+		foreground_width = width;
+		foreground_height = height * percentage;
+
+		background_width = width;
+		background_height = height - foreground_height;
+
+		foreground_shift_y = background_height;
+
+	else -- Left to Right
+		foreground_width = width * percentage;
+		foreground_height = height;
+
+		background_width = width - foreground_width;
+		background_height = height;
+
+		background_shift_x = foreground_width;
 	end
 
 	local foreground_color = bar.colors.foreground;
@@ -197,6 +282,26 @@ function this.draw_bar(bar, position, opacity_scale, percentage)
 
 	local use_d2d = d2d ~= nil and config.current_config.settings.use_d2d_if_available;
 
+	-- background
+	if background_width ~= 0 then
+		if use_d2d then
+			d2d.fill_rect(position_x + background_shift_x, position_y + background_shift_y, background_width, background_height, background_color);
+		else
+			background_color = this.argb_color_to_abgr_color(background_color);
+			draw.filled_rect(position_x + background_shift_x, position_y + background_shift_y, background_width, background_height, background_color)
+		end
+	end
+
+	-- foreground
+	if foreground_width ~= 0 then
+		if use_d2d then
+			d2d.fill_rect(position_x + foreground_shift_x, position_y + foreground_shift_y, foreground_width, foreground_height, foreground_color);
+		else
+			foreground_color = this.argb_color_to_abgr_color(foreground_color);
+			draw.filled_rect(position_x + foreground_shift_x, position_y + foreground_shift_y, foreground_width, foreground_height, foreground_color)
+		end
+	end
+
 	-- outline
 	if outline_thickness ~= 0 then
 		if use_d2d then
@@ -204,26 +309,6 @@ function this.draw_bar(bar, position, opacity_scale, percentage)
 		else
 			outline_color = this.argb_color_to_abgr_color(outline_color);
 			draw.outline_rect(outline_position_x, outline_position_y, outline_width, outline_height, outline_color);
-		end
-	end
-
-	-- foreground
-	if foreground_width ~= 0 then
-		if use_d2d then
-			d2d.fill_rect(position_x, position_y, foreground_width, height, foreground_color);
-		else
-			foreground_color = this.argb_color_to_abgr_color(foreground_color);
-			draw.filled_rect(position_x, position_y, foreground_width, height, foreground_color)
-		end
-	end
-
-	-- background
-	if background_width ~= 0 then
-		if use_d2d then
-			d2d.fill_rect(position_x + foreground_width, position_y, background_width, height, background_color);
-		else
-			background_color = this.argb_color_to_abgr_color(background_color);
-			draw.filled_rect(position_x + foreground_width, position_y, background_width, height, background_color)
 		end
 	end
 end
